a5db83be34f853b0fa3eeb616ab3174ae7d49cb9
LegrandMichael <michael.c.legrand@gmail.com>
2017-11-03 14:03:17 +0100

    Modification de la méthode parler et de la classe Animal en abstrait

--- Animal.php
+++ Animal.php
@@ -1,6 +1,6 @@
 <?php
 
-    class Animal {
+    abstract class Animal {
 
         private $nom;
 
@@ -14,6 +14,12 @@
 
         public function getNom(){
             return $this->nom;
+        }
+
+        abstract public function parler();
+
+        public function __toString() {
+            return get_class($this). " : " .$this->nom;
         }
     }
 
