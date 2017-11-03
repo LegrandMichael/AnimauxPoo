e7d394c9997b9ebc0a2c0ef68e563bb7c7ccb6d2
LegrandMichael <michael.c.legrand@gmail.com>
2017-11-03 14:06:07 +0100

    Suppression du constructeur __construct() situé dans la classe Animal

--- Chien.php
+++ Chien.php
@@ -1,14 +1,11 @@
 <?php
+
     require_once('Animal.php');
+
     class Chien extends Animal {
 
-        public function __construct($nom)
-        {
-            parent::__construct($nom);
-        }
-        
         public function parler() {
-            echo '<div>'.$this->getNom().' dit wouuf</div>';
+            echo '<div>'.$this->getNom().' dit wouuf</div><br>';
         }
 
     }
