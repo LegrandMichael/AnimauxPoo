f2068ab0d22df10272159724d9f004049af88bae
LegrandMichael <michael.c.legrand@gmail.com>
2017-11-03 10:12:08 +0100

    Instanciation de deux objets

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ require_once('Chat.php');
 $granola = new Chat("Granola");
 $granola->parler();
 
-var_dump($granola);
 
 $jasmin = new Chien("Jasmin");
 $jasmin->parler();
 
-var_dump($jasmin);  
+//var_dump($jasmin);  
+//var_dump($granola);
 
 ?>
